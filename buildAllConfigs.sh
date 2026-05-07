b91c7e09c7134b7460645379953035891164c682
Scott Aron Bloom <scott@towel42.com>
2026-05-07 20:38:39 +0100

    Dont run ENABLE_TESTING Split args by -- for before and after

--- buildAllConfigs.sh
+++ buildAllConfigs.sh
@@ -3,6 +3,32 @@
 before=$(compgen -A variable -A function)
 . ~/bin/scripts/buildAllConfigs.sh
 
+declare -a allArgs=("$@")
+declare -a preArgs=()
+declare -a postArgs=()
+
+declare foundDashDash=0
+for ((ii=0; ii<${#allArgs[@]}; ii++)); do
+    currArg=${allArgs[ii]}
+    if [[ "${currArg}" == "--" ]]; then
+        foundDashDash=1
+    fi
+
+    #echo "currArg=|${currArg}| - ${foundDashDash}"
+    if [[ ${foundDashDash} == 0 ]]; then
+        preArgs+=("${currArg}")
+    else
+        postArgs+=("${currArg}")
+    fi
+done
+unset ii
+unset allArgs
+unset currArg
+unset foundDashDash
+
+#echo " preArgs - "${#preArgs[@]}" = ${preArgs[@]}"
+#echo "postArgs - "${#postArgs[@]}" = ${postArgs[@]}"
+
 T42_CMAKEUTILS_DIR=$(cygpath -m $(realpath ../T42-CMakeUtils/))
 declare -a args=(
     "--cmake-option"
@@ -12,7 +38,7 @@ declare -a args=(
     "TOWEL42_GIFSUPPORT"
     "TOWEL42_DESIGNERPLUGIN_SUPPORT"
     "TOWEL42_ZIP_SUPPORT"
-    "TOWEL42_ENABLE_TESTING"
+#    "TOWEL42_ENABLE_TESTING" # this requires others to be on
     "TOWEL42_VSINSTALLER_SUPPORT"
     "TOWEL42_QCONCURRENT_SUPPORT"
     "TOWEL42_QCORE_SUPPORT"
@@ -30,8 +56,12 @@ declare -a args=(
     "Enable forcing qt as a secondary config"
 )
 
-set -- "$@" "${args[@]}" 
+set -- "${preArgs[@]}" "${args[@]}" "${postArgs[@]}"
 
+unset preArgs
+unset postArgs
+
+#echo "$# - $@"
 unset args
 unset T42_CMAKEUTILS_DIR
 
